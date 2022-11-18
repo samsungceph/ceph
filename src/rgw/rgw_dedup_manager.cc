ce40c6e3e4a4eb97802cc407bca71fd89b94eb7b
Sungmin Lee <ssdohammer@gmail.com>
2022-11-18 14:51:22 +0900

    rgw/dedup: RGWDedupManager implementation
    RGWDedupManager executes following functions repeatedly which are setting rados pools,
    getting target objects, and spawning Workers (RGWDedupWorker, and RGWScrubWorker)
    to manage deduplication in RGW.
     - RGWDedupWorker: try deduplication for target objects
     - RGWScrubWorker: fix reference mismatches between metadata and chunk objects
    
    Signed-off-by: Sungmin Lee <sung_min.lee@samsung.com>

--- src/rgw/rgw_dedup_manager.cc
+++ src/rgw/rgw_dedup_manager.cc
@@ -1,12 +1,98 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab ft=cpp
 
+#include <algorithm>
+
 #include "rgw_dedup_manager.h"
+#include "rgw_rados.h"
 
 #define dout_subsys ceph_subsys_rgw
 
+const int RETRY_SLEEP_PERIOD = 30;
+const int DEDUP_INTERVAL = 3;
+const int DEFAULT_NUM_WORKERS = 3;
+const int MAX_OBJ_SCAN_SIZE = 100;
+const int MAX_BUCKET_SCAN_SIZE = 100;
+const int DEFAULT_DEDUP_SCRUB_RATIO = 10;
+const string DEFAULT_COLD_POOL_POSTFIX = "_cold";
+const string DEFAULT_CHUNK_SIZE = "16384";
+const string DEFAULT_CHUNK_ALGO = "fastcdc";
+const string DEFAULT_FP_ALGO = "sha1";
+
 void RGWDedupManager::initialize()
 {
+  for (int i = 0; i < num_workers; ++i) {
+    dedup_workers.emplace_back(
+      make_unique<RGWDedupWorker>(dpp, cct, store, i));
+    scrub_workers.emplace_back(
+      make_unique<RGWChunkScrubWorker>(dpp, cct, store, i, num_workers));
+  }
+}
+
+int RGWDedupManager::append_rados_obj(vector<unique_ptr<RGWDedupWorker>>::iterator& witer,
+                                      const target_rados_object& obj,
+                                      const size_t objs_per_worker,
+                                      int& remain_objs)
+{
+  ceph_assert(witer != dedup_workers.end());
+
+  (*witer)->append_obj(obj);
+  if (((*witer)->get_num_objs() == objs_per_worker)) {
+    // append remain object for even distribution if remain_objs exists
+    if (remain_objs > 0) {
+      --remain_objs;
+      return -1;
+    }
+    ++witer;
+  } else if ((*witer)->get_num_objs() > objs_per_worker) {
+    ++witer;
+  }
+  return 0;
+}
+
+void RGWDedupManager::hand_out_objects()
+{
+  size_t objs_per_worker = rados_objs.size() / num_workers;
+  int remain_objs = rados_objs.size() % num_workers;
+  for (auto& worker: dedup_workers) {
+    worker->clear_objs();
+  }
+
+  vector<unique_ptr<RGWDedupWorker>>::iterator witer = dedup_workers.begin();
+  if (obj_scan_fwd) {
+    for (vector<target_rados_object>::iterator oiter = rados_objs.begin();
+         oiter != rados_objs.end();
+         ++oiter) {
+      if (append_rados_obj(witer, *oiter, objs_per_worker, remain_objs) < 0) {
+        continue;
+      }
+    }
+  } else {  // reverse direction scan
+    for (vector<target_rados_object>::reverse_iterator oiter = rados_objs.rbegin();
+         oiter != rados_objs.rend();
+         ++oiter) {
+      if (append_rados_obj(witer, *oiter, objs_per_worker, remain_objs) < 0) {
+        continue;
+      }
+    }
+  }
+
+  // reverse scanning direction
+  obj_scan_fwd ^= 1;
+}
+
+string RGWDedupManager::create_mon_cmd(const string& prefix,
+                                       const vector<pair<string, string>>& options)
+{
+  ceph_assert(!prefix.empty());
+
+  string cmd("{\"prefix\": \"" + prefix + "\"");
+
+  for (auto& opt : options) {
+    cmd.append(", \"" + opt.first + "\": \"" + opt.second + "\"");
+  }
+  cmd.append("}");
+  return cmd;
 }
 
 void* RGWDedupManager::entry()
@@ -14,7 +100,47 @@ void* RGWDedupManager::entry()
   ldpp_dout(dpp, 2) << "RGWDedupManager started" << dendl;
 
   while (!get_down_flag()) {
-    sleep(3);
+    if (dedup_worked_cnt < dedup_scrub_ratio) {
+      int ret = prepare_dedup();
+      if (ret < 0) {
+        ldpp_dout(dpp, 0) << "prepare_dedup() failed" << dendl;
+        return nullptr;
+      } else if (ret == 0) {
+        ldpp_dout(dpp, 2) << "rados object not found. retry" << dendl;
+        sleep(RETRY_SLEEP_PERIOD);
+        continue;
+      }
+
+      hand_out_objects();
+
+      // trigger RGWDedupWorkers
+      for (auto& worker : dedup_workers) {
+        ceph_assert(worker.get());
+        worker->set_run(true);
+        string name = worker->get_id();
+        worker->create(name.c_str());
+      }
+
+      // all RGWDedupWorkers synchronozed here
+      for (auto& worker: dedup_workers) {
+        worker->join();
+      }
+      ++dedup_worked_cnt;
+    } else {
+      // trigger RGWChunkScrubWorkers
+      for (auto& worker : scrub_workers) {
+        ceph_assert(worker.get());
+        string name = worker->get_id();
+        worker->create(name.c_str());
+      }
+
+      // all RGWChunkScrubWorkers synchronozed here
+      for (auto& worker : scrub_workers) {
+        worker->join();
+      }
+      dedup_worked_cnt = 0;
+    }
+    sleep(DEDUP_INTERVAL);
   }
 
   ldpp_dout(dpp, 2) << "RGWDedupManager is going down" << dendl;
@@ -29,5 +155,192 @@ void RGWDedupManager::stop()
 
 void RGWDedupManager::finalize()
 {
+  for (int i = 0; i < num_workers; ++i) {
+    dedup_workers[i].reset();
+    scrub_workers[i].reset();
+  }
+  dedup_workers.clear();
+  scrub_workers.clear();
+}
+
+int RGWDedupManager::append_ioctxs(rgw_pool base_pool)
+{
+  RGWRados* rados = store->getRados();
+  ceph_assert(rados);
+
+  librados::IoCtx base_ioctx;
+  int ret = rgw_init_ioctx(dpp, rados->get_rados_handle(), base_pool,
+                           base_ioctx, true, false);
+  if (ret < 0) {
+    ldpp_dout(dpp, 0) << "ERROR: failed to get_or_create ioctx pool="
+      << base_pool.name << dendl;
+    return ret;
+  }
+
+  librados::IoCtx cold_ioctx;
+  ret = rgw_init_ioctx(dpp, rados->get_rados_handle(),
+                       rgw_pool(base_pool.name + cold_pool_postfix), 
+                       cold_ioctx, true, false);
+  if (ret < 0) {
+    ldpp_dout(dpp, 0) << "ERROR: failed to get_or_create ioctx pool="
+      << base_pool.name + cold_pool_postfix << dendl;
+    return ret;
+  }
+
+  dedup_ioctx_set pool_set{base_ioctx, cold_ioctx};
+  ioctx_map.insert({base_pool.name, pool_set});
+  return 0;
+}
+
+string RGWDedupManager::create_osd_pool_set_cmd(const string prefix, const string base_pool,
+                                                const string var, const string val)
+{
+  vector<pair<string, string>> options;
+  options.emplace_back(make_pair("pool", base_pool));
+  options.emplace_back(make_pair("var", var));
+  options.emplace_back(make_pair("val", val));
+  return create_mon_cmd(prefix, options);
+}
+
+void RGWDedupManager::set_dedup_tier(string base_pool_name)
+{
+  auto handle = store->svc()->rados->handle();
+  string cold_pool_name = base_pool_name + cold_pool_postfix;
+
+  string cmd = create_osd_pool_set_cmd("osd pool set", base_pool_name, "dedup_tier",
+                                       cold_pool_name);
+  ldpp_dout(dpp, 0) << __func__ << " cmd: " << cmd << dendl;
+  if (handle.mon_command(cmd, bufferlist(), nullptr, nullptr) < 0) {
+    ldpp_dout(dpp, 0) << __func__ << " mon_command " << cmd << " failed" << dendl;
+  }
+
+  cmd = create_osd_pool_set_cmd("osd pool set", base_pool_name,
+                                "dedup_chunk_algorithm", chunk_algo);
+  ldpp_dout(dpp, 0) << __func__ << " cmd: " << cmd << dendl;
+  if (handle.mon_command(cmd, bufferlist(), nullptr, nullptr) < 0) {
+    ldpp_dout(dpp, 0) << __func__ << " mon_command " << cmd << " failed" << dendl;
+  }
+
+  cmd = create_osd_pool_set_cmd("osd pool set", base_pool_name,
+                                "dedup_cdc_chunk_size", chunk_size);
+  ldpp_dout(dpp, 0) << __func__ << " cmd: " << cmd << dendl;
+  if (handle.mon_command(cmd, bufferlist(), nullptr, nullptr) < 0) {
+    ldpp_dout(dpp, 0) << __func__ << " mon_command " << cmd << " failed" << dendl;
+  }
+
+  cmd = create_osd_pool_set_cmd("osd pool set", base_pool_name,
+                                "fingerprint_algorithm", fp_algo);
+  ldpp_dout(dpp, 0) << __func__ << " cmd: " << cmd << dendl;
+  if (handle.mon_command(cmd, bufferlist(), nullptr, nullptr) < 0) {
+    ldpp_dout(dpp, 0) << __func__ << " mon_command " << cmd << " failed" << dendl;
+  }
+}
+
+int RGWDedupManager::get_rados_objects(RGWRados::Object::Stat& stat_op)
+{
+  int ret = stat_op.stat_async(dpp);
+  if (ret < 0) {
+    ldpp_dout(dpp, 0) << "ERROR: stat_async() returned error: " <<
+      cpp_strerror(-ret) << dendl;
+    return ret;
+  }
+  ret = stat_op.wait(dpp);
+  if (ret < 0) {
+    if (ret != -ENOENT) {
+      ldpp_dout(dpp, 0) << "ERROR: stat_async() returned error: " <<
+        cpp_strerror(-ret) << dendl;
+    }
+    // clear error. continue processing directory
+    ret = 0;
+  }
+  return ret;
+}
+
+int RGWDedupManager::prepare_dedup()
+{
+  void* handle = nullptr;
+  bool has_remain_bkts = true;
+  int total_obj_cnt = 0;
+
+  rados_objs.clear();
+  int ret = store->meta_list_keys_init(dpp, "bucket", string(), &handle);
+  if (ret < 0) {
+    ldpp_dout(dpp, 0) << "ERROR: meta_list_keys_init() failed" << dendl;
+    return ret;
+  }
+
+  while (has_remain_bkts) {
+    list<string> bucket_list;
+    ret = store->meta_list_keys_next(dpp, handle, MAX_BUCKET_SCAN_SIZE,
+                                     bucket_list, &has_remain_bkts);
+    if (ret < 0) {
+      ldpp_dout(dpp, 0) << "ERROR: meta_list_keys_next() failed" << dendl;
+      store->meta_list_keys_complete(handle);
+      return ret;
+    }
+    for (auto bucket_name : bucket_list) {
+      unique_ptr<rgw::sal::Bucket> bucket;
+      ret = store->get_bucket(dpp, nullptr, "", bucket_name, &bucket, null_yield);
+      if (ret < 0) {
+        ldpp_dout(dpp, 0) << "ERROR: get_bucket() failed" << dendl;
+        store->meta_list_keys_complete(handle);
+        return ret;
+      }
+
+      rgw::sal::Bucket::ListParams params;
+      rgw::sal::Bucket::ListResults results;
+      bool has_remain_objs = true;
+      const string bucket_id = bucket->get_key().get_key();
+      while (has_remain_objs) {
+        ret = bucket->list(dpp, params, MAX_OBJ_SCAN_SIZE, results, null_yield);
+        if (ret < 0) {
+          ldpp_dout(dpp, 0) << "ERROR: list() failed" << dendl;
+          store->meta_list_keys_complete(handle);
+          return ret;
+        }
+
+        for (auto obj : results.objs) {
+          ldpp_dout(dpp, 10) << "rgw_obj name: " << obj.key.name << dendl;
+          RGWObjectCtx obj_ctx(store);
+          unique_ptr<rgw::sal::Object> rgw_obj = bucket->get_object(obj.key);
+          RGWRados::Object op_target(store->getRados(), bucket.get(),
+                                     obj_ctx, rgw_obj.get());
+          RGWRados::Object::Stat stat_op(&op_target);
+          ret = get_rados_objects(stat_op);
+          if (ret < 0) {
+            store->meta_list_keys_complete(handle);
+            return ret;
+          }
+
+          RGWObjManifest& manifest = *stat_op.result.manifest;
+          RGWObjManifest::obj_iterator miter;
+          for (miter = manifest.obj_begin(dpp);
+               miter != manifest.obj_end(dpp);
+               ++miter) {
+            const rgw_raw_obj& rados_obj
+              = miter.get_location()
+                     .get_raw_obj(static_cast<rgw::sal::RadosStore*>(store));
+            if (rados_obj == rgw_raw_obj()) {
+              continue;
+            }
+
+            string base_pool_name = rados_obj.pool.name;
+            if (ioctx_map.find(base_pool_name) == ioctx_map.end()) {
+              if (append_ioctxs(rados_obj.pool) < 0) {
+                continue;
+              }
+              set_dedup_tier(base_pool_name);
+            }
+            ++total_obj_cnt;
+          }
+        }
+
+        has_remain_objs = results.is_truncated;
+      }
+    }
+  }
+  store->meta_list_keys_complete(handle);
+
+  return total_obj_cnt;
 }
 
--- src/rgw/rgw_dedup_manager.h
+++ src/rgw/rgw_dedup_manager.h
@@ -8,30 +8,104 @@
 #include "common/Cond.h"
 #include "common/Thread.h"
 #include "rgw_sal_rados.h"
-
+#include "rgw_dedup_worker.h"
 
 using namespace std;
+using namespace librados;
 
+extern const string DEFAULT_CHUNK_POOL_POSTFIX;
+extern const string DEFAULT_COLD_POOL_POSTFIX;
+extern const string DEFAULT_CHUNK_SIZE;
+extern const string DEFAULT_CHUNK_ALGO;
+extern const string DEFAULT_FP_ALGO;
+extern const int DEFAULT_NUM_WORKERS;
+extern const int DEFAULT_DEDUP_SCRUB_RATIO;
+
+struct target_rados_object {
+  string object_name;
+  string pool_name;
+};
+
+class RGWDedupWorker;
+class RGWChunkScrubWorker;
 class RGWDedupManager : public Thread
 {
   const DoutPrefixProvider* dpp;
   CephContext* cct;
   rgw::sal::RadosStore* store;
   bool down_flag;
+  vector<target_rados_object> rados_objs;
+
+  vector<unique_ptr<RGWDedupWorker>> dedup_workers;
+  vector<unique_ptr<RGWChunkScrubWorker>> scrub_workers;
+
+  string cold_pool_postfix;
+  string chunk_size;
+  string chunk_algo;
+  string fp_algo;
+  int num_workers;
+  int dedup_scrub_ratio;
+  int dedup_worked_cnt;
+  bool obj_scan_fwd;    // true: scan rados_objs forward, false: scan reverse
+
+  /**
+   *  There is a data_pool which is regarded as base-pool for a storage_classes.
+   *  For dedup, a chunk-pool and a cold-pool are required for each base-pool.
+   *  struct dedup_ioctx_set indicates the IoCtxs of the pools related to each other.
+   */
+  struct dedup_ioctx_set {
+    IoCtx base_pool_ctx;
+    IoCtx cold_pool_ctx;
+  };
+  map<string, dedup_ioctx_set> ioctx_map; // SC data pool (base-pool name):ioctxs
 
 public:
   RGWDedupManager(const DoutPrefixProvider* _dpp,
                  CephContext* _cct,
                  rgw::sal::RadosStore* _store)
-    : dpp(_dpp), cct(_cct), store(_store), down_flag(true) {}
-  ~RGWDedupManager() {}
-  void* entry() override;
+    : dpp(_dpp), cct(_cct), store(_store), down_flag(true),
+      cold_pool_postfix(DEFAULT_COLD_POOL_POSTFIX),
+      chunk_size(DEFAULT_CHUNK_SIZE),
+      chunk_algo(DEFAULT_CHUNK_ALGO),
+      fp_algo(DEFAULT_FP_ALGO),
+      num_workers(DEFAULT_NUM_WORKERS),
+      dedup_scrub_ratio(DEFAULT_DEDUP_SCRUB_RATIO),
+      dedup_worked_cnt(0),
+      obj_scan_fwd(true) {}
+  virtual ~RGWDedupManager() override {}
+  virtual void* entry() override;
+
   void stop();
   void finalize();
   bool going_down();
   void initialize();
   void set_down_flag(bool new_flag) { down_flag = new_flag; }
   bool get_down_flag() { return down_flag; }
+  size_t get_num_rados_obj() { return rados_objs.size(); }
+  void set_dedup_tier(string base_pool_name);
+  int append_ioctxs(rgw_pool base_pool);
+  int get_rados_objects(RGWRados::Object::Stat& stat_op);
+
+  // add rados obj to Worker referring to objs_per_worker and update remain_objs
+  int append_rados_obj(vector<unique_ptr<RGWDedupWorker>>::iterator& witer,
+                       const target_rados_object& obj,
+                       const size_t objs_per_worker,
+                       int& remtin_objs);
+
+  // distribute aggregated rados objects evenly to each RGWDedupWorker
+  // reverse scanning rados_objs direction to avoid an issue that several objects
+  // processed in an early stage can not be deduped because of insufficient fp info
+  void hand_out_objects();
+
+  // get all rados objects to deduplicate
+  int prepare_dedup();
+
+  // get all chunk objects to scrub
+  int prepare_scrub();
+
+  string create_mon_cmd(const string& prefix, const vector<pair<string, string>>& options);
+  string create_osd_pool_set_cmd(const string prefix, const string base_pool,
+                                 const string var, const string val);
 };
 
 #endif
